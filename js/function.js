cf6d8af96a9740091b6df0720d075938926c1ddb
Kirill Tretiakov <kirillgti@bk.ru>
2024-02-22 18:11:21 +0300

    alternative tasks

--- js/function.js
+++ js/function.js
@@ -57,5 +57,65 @@ function wtf(string) {
 
 }
 
-console.log(wtf('Сегодня 20 февраля 2024 года, за окном -2.5 градуса. Агент 007 провалил задание, т.к. yне понял как еще вернуть NaN при нечисловой строке'));
+console.log(wtf('Сегодня 20 февраля 2024 года, за окном -2.5 градуса. Агент 007 провалил задание, т.к. не понял как по-другому вернуть NaN при нечисловой строке'));
+
+
+//Вариант решения задания с тренажёра:
+
+/*let qualificationDistance = 200;
+let attempts = [120, 150, 160, 201, 203, 180, 202];
+let qualified = false;
+let averageBest = 0;
+let attemptQuantitys = 3;
+
+
+for (let i = 0; i <= attempts.length - 2; i++) {
+  let bestAttempt = attempts[i];
+
+  for (let j = i + 1; j <= attempts.length - 1; j++){
+    if (bestAttempt > attempts[j]) {
+      bestAttempt = attempts[j];
+      let swap = attempts[i];
+      attempts[i] = bestAttempt;
+      attempts[j] = swap;
+    }
+  }
+}
+
+console.log(attempts);
+
+let attemptsSum = 0;
+
+for (let i = 1; i <= attemptQuantitys; i++) {
+  attemptsSum += attempts[attempts.length - i]
+}
+
+averageBest = attemptsSum / attemptQuantitys;
+
+if (averageBest > qualificationDistance) {
+  qualified = true;
+}
+*/
+
+
+//Альтернативное решение (Перебором находять max значеня и добавлять их в массив):
+
+let attempts = [120, 150, 160, 201, 203, 180, 202, 190, 195];
+let bestAttempt = 0;
+let attemptsBest = [];
+let quantityBestAttempts = 3;
+
+for (let j = 0; j <= quantityBestAttempts - 1; j++) {
+
+  for (let i = 0; i <= attempts.length - 1; i++) {
+    let isBestAttempt = attempts[i];
+    if (isBestAttempt < attempts[i + 1] && attempts[i + 1] > bestAttempt) {
+      bestAttempt = attempts[i + 1];
+    }
+  }
+
+  attemptsBest[j] = bestAttempt;
+}
+
+console.log(attemptsBest);
 
