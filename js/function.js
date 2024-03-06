75762ee0442778e86f076251955a19c277b9c83e
Kirill Tretiakov <kirillgti@bk.ru>
2024-03-06 16:45:32 +0300

    module5-task2

--- js/function.js
+++ js/function.js
@@ -96,165 +96,31 @@ if (averageBest > qualificationDistance) {
   qualified = true;
 }
 */
-//============================================
 
-const photoDescriptions = [
-  'Коллекция морских ракушек',
-  'КотОстрофа',
-  'Альтернативные способы заварки кофе',
-  'Выходные с пользой',
-  'Рыбалка в Мурманске',
-  '1 мая - день шашлыков',
-  'Прогулка по зимнему лесу',
-  'Петербуржский минимализм',
-  'Букет на 8 марта',
-  'Банные процедуры',
-  'Нам 1 годик',
-  'Зимнее эндуро',
-  'Коралловый риф и его обитатели',
-  'Генератор случайных фраз',
-  'Коллекция советских учебников по политической экономии',
-  'Новый аквариум',
-  'Массивные объекты в архитектуре Уругвая',
-  'Звёздное небо',
-  'Кино-вечер',
-  'Мороз и солнце...',
-  'Презентация нового трека',
-  'Распродажа зимней резины',
-  'Пополнение коллекции',
-  'Сегодня ходили на волков',
-  '25-й пост'
-];
 
-const commentMessages = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-];
+//========== MODULE5-TASK2 ==============
 
-const commentNames = [
-  'Васисуасилий Камушкин',
-  'Познер АК47',
-  'Линкольн Викторович',
-  'Агент трикота',
-  'Рик Санчез',
-  'Гари Янгмэн',
-  'Мистер Бин',
-  'NEO',
-  'Убийца цветочной луны',
-  'Гэри Гудспид',
-  'Кат Матроцкин',
-  'Капитан Очевидность',
-  'Джесан Стэтан',
-  'Леопольд',
-  'Кривда Васичкин',
-  'Пшек Пшекович',
-  'Бобр Добр'
-];
-
-/*Функция генерации случайных чисел*/
-
-const getRandomInt = (minInt, maxInt) => {
-  const randomInt = Math.floor(Math.random() * (maxInt - minInt + 1) + minInt);
-  return randomInt;
+const getTimePerHour = (time) => {
+  const timeArr = time.split(':');
+  timeArr[1] = timeArr[1] / 60;
+  const timePerHour = timeArr.reduce((accumulator, currentValue) => accumulator + +currentValue,0);
+  return timePerHour;
 };
 
-/*Функция генерации уникальных чисел в заданном диапазоне + проверка на уникальность*/
+const eventsPossibility = (startTime, endTime, startEventTime, iventDuration) => {
+  const startTimePerHour = getTimePerHour(startTime);
+  const endTimePerHour = getTimePerHour(endTime);
+  const startEventTimePerHour = getTimePerHour(startEventTime);
+  const endEventTimePerHour = startEventTimePerHour + iventDuration / 60;
 
-const getUniqueId = (minInt, maxInt) => {
-  const uniqueIds = {};
-
-  return () => {
-    let uniqueId = getRandomInt(minInt, maxInt);
-    if (uniqueIds[uniqueId] === true) {
-      uniqueId = getRandomInt(minInt, maxInt);
-    }
-    uniqueIds[uniqueId] = true;
-
-    return uniqueId;
-  };
-};
-
-/*Генерация комментария*/
-
-const commentIdMinValue = 1;
-const commentIdMaxValue = 30;
-
-const commentUniqueId = getUniqueId(commentIdMinValue, commentIdMaxValue);
-
-const getComment = () => {
-  const id = commentUniqueId();
-  const avatar = `img/avatar-${getRandomInt(1, 6)}.svg`;
-  const message = commentMessages[getRandomInt(0, commentMessages.length - 1)];
-  const name = commentNames[getRandomInt(0, commentNames.length - 1)];
-
-  return {
-    id,
-    avatar,
-    message,
-    name,
-  };
-};
-
-/*Добавление комментариев в массив*/
-
-const getCommentsArr = () => {
-  const commentsArr = [];
-  const commentsCount = getRandomInt(0, 30);
-  while (commentsArr.length <= commentsCount) {
-    commentsArr.push(getComment());
+  if (startTimePerHour <= startEventTimePerHour && endTimePerHour >= endEventTimePerHour) {
+    return true;
   }
-
-  return commentsArr;
+  return false;
 };
 
-/*Генерация фото*/
-
-const photoIdMinValue = 1;
-const photoIdMaxValue = 25;
-const photoUniqueId = getUniqueId(photoIdMinValue, photoIdMaxValue);
-
-const urlIdMinValue = 1;
-const urlIdMaxValue = 25;
-const urlUniqueId = getUniqueId(urlIdMinValue, urlIdMaxValue);
-
-const getPhoto = () => {
-
-  const id = photoUniqueId();
-
-
-  const url = `photos/${urlUniqueId()}.jpg`;
-
-  const description = photoDescriptions[getRandomInt(0, photoDescriptions.length - 1)];
-  const likes = getRandomInt(15, 250);
-  const comments = getCommentsArr();
-
-  return {
-    id,
-    url,
-    description,
-    likes,
-    comments
-  };
-};
-
-/*Добавление фотографий в массив*/
-
-const getPhotosArr = () => {
-  const photosArr = [];
-  while (photosArr.length < 25) {
-    photosArr.push(getPhoto());
-  }
-
-  return photosArr;
-};
-
-console.log(getPhotosArr());
-
-
-
-
-
+console.log(eventsPossibility('08:00', '17:30', '14:00', 90));
+console.log(eventsPossibility('8:0', '10:0', '8:0', 120));
+console.log(eventsPossibility('08:00', '14:30', '14:00', 90));
+console.log(eventsPossibility('14:00', '17:30', '08:0', 90));
+console.log(eventsPossibility('8:00', '17:30', '08:00', 900));
